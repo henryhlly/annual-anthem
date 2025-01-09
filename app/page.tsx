9c4311a162c937c7e10b21df39df2796cc7d550b
Henry Ly <lyhenry2004@gmail.com>
2025-01-10 02:41:21 +1300

    Fixed positioning of home page (not responsive)

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      {/* Description textbox */}
       <div className="fixed top-64 left-32 bg-stone-700 py-10 px-5 max-w-md rounded-lg flex flex-col gap-5">
         <h1 className="text-white text-2xl">
           Find your ultimate song of 2024!
@@ -11,16 +12,16 @@ export default function Home() {
           Go through a bracket style tournament system where you have the power, and determine your favourite songs of the past year.
         </p>
       </div>
-      <div className="flex flex-col w-full py-40 max-w-screen-sm min-h-screen h-auto m-auto mr-20 items-center gap-16">
+      <div className="flex flex-col ml-96 py-32 min-h-screen items-center justify-center gap-32">
         <div>
           <h1 className="text-4xl text-center font-medium">
-            Kpop
+        General
           </h1>
-          <Image src="/kpop-logo.png" alt="kpop logo" width={300} height={300} quality={100} />
+          <Image src="/general-logo.png" alt="general logo" width={300} height={300} quality={100} />
         </div>
         <div>
           <h1 className="text-4xl text-center font-medium">
-            Kpop
+        Kpop
           </h1>
           <Image src="/kpop-logo.png" alt="kpop logo" width={300} height={300} quality={100} />
         </div>
