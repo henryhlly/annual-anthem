1425541cead00d9accc72ca30367012c489eb8fb
Henry Ly <lyhenry2004@gmail.com>
2025-01-10 20:11:09 +1300

    Changed the images around after feedback

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       {/* Description textbox */}
-      <div className="fixed top-64 left-32 bg-stone-700/50 py-10 px-5 max-w-md rounded-lg flex flex-col gap-5 shadow-lg">
-        <h1 className="text-white text-2xl">
+      <div className="fixed top-48 left-32 bg-stone-700/50 py-16 px-16 max-w-md rounded-lg flex flex-col gap-5 shadow-lg">
+        <h1 className="text-white text-3xl">
           Find your ultimate song of 2024!
         </h1>
         <p className="text-white text-sm">
@@ -13,17 +13,17 @@ export default function Home() {
         </p>
       </div>
       <div className="flex flex-col ml-96 py-32 min-h-screen items-center justify-center gap-32">
-        <div>
+        <div className="flex flex-col -gap-16">
           <h1 className="text-4xl text-center font-medium">
         General
           </h1>
-          <Image src="/general-logo.png" alt="general logo" width={300} height={300} quality={100} />
+          <Image src="/kpop-logo.png" alt="general logo" width={400} height={400} quality={100} />
         </div>
-        <div>
+        <div className="flex flex-col -gap-16">
           <h1 className="text-4xl text-center font-medium">
         Kpop
           </h1>
-          <Image src="/kpop-logo.png" alt="kpop logo" width={300} height={300} quality={100} />
+          <Image src="/general-logo.png" alt="kpop logo" width={400} height={400} quality={100} />
         </div>
       </div>
     </main>
