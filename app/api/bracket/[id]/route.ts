fd0b2874f4bfd185fe3ee9d5bbf7bcfd33688de3
henryhlly <lyhenry2004@gmail.com>
2025-01-17 22:16:41 +1300

    Began implementing genre specific brackets

---
+++ app/api/bracket/[id]/route.ts
@@ -0,0 +1,19 @@
+import genres from '@/data/genres.json';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  try {
+    const { id } = await params
+    const genre = genres.data.find(item => item.id === id)
+
+    if (!genre) {
+      return new NextResponse('not found', { status: 404 })
+    }
+
+    const data = await import(`@/data/${genre.data}.json`);
+
+    return NextResponse.json({ data: data.default });
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
