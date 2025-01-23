9e080c4448dede3dcbc809a925d2d49e90042c4e
henryhlly <lyhenry2004@gmail.com>
2025-01-23 20:58:01 +1300

    Finished basic run through of app after mongo integration

--- app/result/[id]/[youtube_url]/page.tsx
+++ app/result/[id]/[youtube_url]/page.tsx
@@ -7,7 +7,7 @@ export default async function Page({ params }: { params: { id: string, youtube_u
   const { id, youtube_url } = await params
 
   const genre = await getGenreById({ genreId: id })
-  const song = await getSongByUrl({ genreData: genre.genre.data, url: youtube_url });
+  const song = await getSongByUrl({ genreData: genre.data, url: youtube_url });
 
   return (
     <main className="flex flex-col items-center h-[calc(100vh-4rem)] w-full py-6 gap-8">
