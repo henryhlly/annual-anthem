dbf869384f3ff51dde76accb0cff81d457d74035
joengy <joengu4130@gmail.com>
2025-01-24 16:31:19 +1300

    completed the css for the arrows so that they look like actual arrows

--- app/component/EmblaCarousel.tsx
+++ app/component/EmblaCarousel.tsx
@@ -1,37 +1,55 @@
-import useEmblaCarousel from 'embla-carousel-react'
-import Image from 'next/image'
-import React, { useCallback } from 'react'
-import {
-    PrevButton,
-    NextButton,
-    usePrevNextButtons
-  } from './EmblaCarouselArrowButtons'
+import useEmblaCarousel from 'embla-carousel-react';
+import Image from 'next/image';
+import React, { useCallback } from 'react';
 
-  export function EmblaCarousel() {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
-    const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi)
+export function EmblaCarousel() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
-    const scrollPrev = useCallback(() => {
-        if (emblaApi) emblaApi.scrollPrev()
-    }, [emblaApi])
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
-    const scrollNext = useCallback(() => {
-        if (emblaApi) emblaApi.scrollNext()
-    }, [emblaApi])
-
-
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
     <div>
       <div className="embla" ref={emblaRef}>
         <div className="embla__container">
-          <div className="embla__slide"><Image src="/general-logo.png" alt="general logo" width={500} height={500} quality={100} /></div>
-          <div className="embla__slide"><Image src="/kpop-logo.png" alt="kpop logo" width={500} height={500} quality={100} /></div>
+          <div className="embla__slide">
+            <Image src="/general-logo.png" alt="general logo" width={500} height={500} quality={100} />
+          </div>
+          <div className="embla__slide">
+            <Image src="/kpop-logo.png" alt="kpop logo" width={500} height={500} quality={100} />
+          </div>
           <div className="embla__slide">Slide 3</div>
         </div>
       </div>
-            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      <div className="grid grid-cols-2 gap-2.5 items-center">
+        <button
+          className="appearance-none bg-transparent touch-manipulation inline-flex cursor-pointer border-0 p-0 m-0 shadow-inner w-14 h-14 z-10 rounded-full text-current flex items-center justify-center"
+          onClick={scrollPrev}
+        >
+          <svg className="w-1/3 h-1/3" viewBox="0 0 532 532">
+            <path
+              fill="currentColor"
+              d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
+            />
+          </svg>
+        </button>
+        <button
+          className="appearance-none bg-transparent touch-manipulation inline-flex cursor-pointer border-0 p-0 m-0 shadow-inner w-14 h-14 z-10 rounded-full text-current flex items-center justify-center"
+          onClick={scrollNext}
+        >
+          <svg className="w-1/3 h-1/3" viewBox="0 0 532 532">
+            <path
+              fill="currentColor"
+              d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
