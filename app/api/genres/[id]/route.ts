94d1dde333bf916fa664606ce81f4e21565811f7
henryhlly <lyhenry2004@gmail.com>
2025-01-30 16:51:10 +1300

    Ready for another deployment build attempt

--- app/api/genres/[id]/route.ts
+++ app/api/genres/[id]/route.ts
@@ -2,7 +2,7 @@ import { connectToCollection } from '@/lib/mongodb';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function GET(req: NextRequest, { params }: { params: { id: string }}) {
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }>}) {
   const { id } = await params
   const collection = await connectToCollection({ collectionName: "genres" })
   const data = await collection.find({ id: id }).toArray();
