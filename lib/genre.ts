da5c71719f587eec8d401e788b871d1e01e06734
henryhlly <lyhenry2004@gmail.com>
2025-01-31 01:36:32 +1300

    Revert "Tested a successful build"
    This reverts commit 142f6e6d80b0f0182433964ec24fa53d9a973af2.

--- lib/genre.ts
+++ lib/genre.ts
@@ -1,5 +1,3 @@
-import { endpoint } from "@/utils/endpoint";
-
 export type Genre = {
   id: string;
   name: string;
@@ -7,34 +5,34 @@ export type Genre = {
   image: string;
 }
 
-export async function getAllGenres() {
-  const data = await fetch(`${endpoint}/genres`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch all genres')
-  }
-  return data.json()
-}
-
-export async function getGenreById({ genreId }: { genreId: string }) {
-  const data = await fetch(`${endpoint}/genres/${genreId}`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch genre by provided ID')
-  }
-  return data.json()
-}
-
-// import { connectToCollection } from './mongodb';
-
 // export async function getAllGenres() {
-//   const collection = await connectToCollection({ collectionName: "genres" })
-//   const data = await collection.find({}, { projection: { _id: 0 } }).toArray();
-//   return data;
+//   const data = await fetch(`${endpoint}/genres`)
+
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch all genres')
+//   }
+//   return data.json()
 // }
 
 // export async function getGenreById({ genreId }: { genreId: string }) {
-//   const collection = await connectToCollection({ collectionName: "genres" })
-//   const data = await collection.find({ id: genreId }, { projection: { _id: 0 } }).toArray();
-//   return data[0];
+//   const data = await fetch(`${endpoint}/genres/${genreId}`)
+
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch genre by provided ID')
+//   }
+//   return data.json()
 // }
+
+import { connectToCollection } from './mongodb';
+
+export async function getAllGenres() {
+  const collection = await connectToCollection({ collectionName: "genres" })
+  const data = await collection.find({}, { projection: { _id: 0 } }).toArray();
+  return data;
+}
+
+export async function getGenreById({ genreId }: { genreId: string }) {
+  const collection = await connectToCollection({ collectionName: "genres" })
+  const data = await collection.find({ id: genreId }, { projection: { _id: 0 } }).toArray();
+  return data[0];
+}
--- lib/song.ts
+++ lib/song.ts
@@ -1,5 +1,4 @@
 import { shuffle } from '@/lib/shuffle';
-import { endpoint } from '@/utils/endpoint';
 
 export type Song = {
   title: string;
@@ -8,80 +7,80 @@ export type Song = {
   wins: number;
 }
 
-export async function getAllSongs({ genreData }: {genreData: string}) {
-  const data = await fetch(`${endpoint}/songs/${genreData}`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch all songs')
-  }
-  return data.json()
-}
-
-export async function getRandomAllSongs({ genreData, numberOfSongs }: { genreData: string, numberOfSongs: number}) {
-  const data = await getAllSongs({ genreData: genreData })
-  const youtubeUrls: string[] = data.map((song: Song) => song.youtube_url)
-  return shuffle({ array: youtubeUrls }).splice(0, numberOfSongs);
-}
-
-export async function getSongByUrl({ genreData, url }: { genreData: string, url: string }) {
-  const data = await fetch(`${endpoint}/songs/${genreData}/${url}`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch song by provided URL')
-  }
-  return data.json()
-}
-
-
-export async function getHighestWin({ genreData }: {genreData: string}) {
-  const data = await fetch(`${endpoint}/result/${genreData}`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch all songs')
-  }
-  return data.json()
-}
-
-export async function addSongWin({ genreData, url }: {genreData: string, url: string}) {
-  await fetch(`/api/songs/${genreData}/${url}`, { method: 'POST' });
-}
-
-// import { connectToCollection } from './mongodb';
-
 // export async function getAllSongs({ genreData }: {genreData: string}) {
-//   const collection = await connectToCollection({ collectionName: genreData })
-//   const data = await collection.find({}, { projection: { _id: 0 } }).toArray();
-//   return data;
+//   const data = await fetch(`${endpoint}/songs/${genreData}`)
+
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch all songs')
+//   }
+//   return data.json()
 // }
 
 // export async function getRandomAllSongs({ genreData, numberOfSongs }: { genreData: string, numberOfSongs: number}) {
-//   const collection = await connectToCollection({ collectionName: genreData })
-//   const data = await collection.find({}, { projection: { _id: 0 } }).toArray();
-//   const youtubeUrls: string[] = data.map((song) => (song).youtube_url);
+//   const data = await getAllSongs({ genreData: genreData })
+//   const youtubeUrls: string[] = data.map((song: Song) => song.youtube_url)
 //   return shuffle({ array: youtubeUrls }).splice(0, numberOfSongs);
 // }
 
 // export async function getSongByUrl({ genreData, url }: { genreData: string, url: string }) {
-//   const collection = await connectToCollection({ collectionName: genreData })
-//   const data = await collection.find({ youtube_url: url }, { projection: { _id: 0 } }).toArray();
-//   return data[0];
+//   const data = await fetch(`${endpoint}/songs/${genreData}/${url}`)
+
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch song by provided URL')
+//   }
+//   return data.json()
 // }
 
+
 // export async function getHighestWin({ genreData }: {genreData: string}) {
-//     const collection = await connectToCollection({ collectionName: genreData })
-//     const result = await collection.find({}, { projection: { _id: 0 } }).sort({ wins: -1 }).toArray();
-//     const data: Song[] = result.map((song) => ({
-//         title: song.title,
-//         artist: song.artist,
-//         youtube_url: song.youtube_url,
-//         wins: song.wins
-//     }));
-//     return data;
+//   const data = await fetch(`${endpoint}/result/${genreData}`)
+
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch all songs')
+//   }
+//   return data.json()
 // }
 
 // export async function addSongWin({ genreData, url }: {genreData: string, url: string}) {
-//   const collection = await connectToCollection({ collectionName: genreData })
-//   const song = await collection.find({ youtube_url: url }, { projection: { _id: 0 } }).toArray();
-//   const numWins = song[0].wins
-//   collection.updateOne({ youtube_url: url }, { $set: { wins: numWins + 1 }})
+//   await fetch(`/api/songs/${genreData}/${url}`, { method: 'POST' });
 // }
+
+import { connectToCollection } from './mongodb';
+
+export async function getAllSongs({ genreData }: {genreData: string}) {
+  const collection = await connectToCollection({ collectionName: genreData })
+  const data = await collection.find({}, { projection: { _id: 0 } }).toArray();
+  return data;
+}
+
+export async function getRandomAllSongs({ genreData, numberOfSongs }: { genreData: string, numberOfSongs: number}) {
+  const collection = await connectToCollection({ collectionName: genreData })
+  const data = await collection.find({}, { projection: { _id: 0 } }).toArray();
+  const youtubeUrls: string[] = data.map((song) => (song).youtube_url);
+  return shuffle({ array: youtubeUrls }).splice(0, numberOfSongs);
+}
+
+export async function getSongByUrl({ genreData, url }: { genreData: string, url: string }) {
+  const collection = await connectToCollection({ collectionName: genreData })
+  const data = await collection.find({ youtube_url: url }, { projection: { _id: 0 } }).toArray();
+  return data[0];
+}
+
+export async function getHighestWin({ genreData }: {genreData: string}) {
+    const collection = await connectToCollection({ collectionName: genreData })
+    const result = await collection.find({}, { projection: { _id: 0 } }).sort({ wins: -1 }).toArray();
+    const data: Song[] = result.map((song) => ({
+        title: song.title,
+        artist: song.artist,
+        youtube_url: song.youtube_url,
+        wins: song.wins
+    }));
+    return data;
+}
+
+export async function addSongWin({ genreData, url }: {genreData: string, url: string}) {
+  const collection = await connectToCollection({ collectionName: genreData })
+  const song = await collection.find({ youtube_url: url }, { projection: { _id: 0 } }).toArray();
+  const numWins = song[0].wins
+  collection.updateOne({ youtube_url: url }, { $set: { wins: numWins + 1 }})
+}
