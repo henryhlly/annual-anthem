94ed37b78ceb8b8202c0d9cc73ff08bcb5da1abf
henryhlly <lyhenry2004@gmail.com>
2025-02-07 00:04:50 +1300

    Implemented responsiveness to the results page

--- app/result/[id]/[youtube_url]/page.tsx
+++ app/result/[id]/[youtube_url]/page.tsx
@@ -13,12 +13,12 @@ export default async function Page({ params }: { params: Promise<{ id: string, y
   const songData = await getHighestWin({ genreData: genre.data })
 
   return (
-    <main className="flex flex-row gap-2">
-      <div className="flex flex-col items-center h-[calc(100vh-4rem)] w-full py-6 gap-8">
+    <main className="flex lg:flex-row flex-col gap-2 justify-center min-h-[calc(100vh-4rem)] items-center lg:items-start">
+      <div className="flex flex-col items-center w-full py-6 gap-8">
         <Result title={song.title} artist={song.artist} youtube_url={song.youtube_url} />
         <Button href={"/"} text={"Back to Home"} hasArrow={true}/>
       </div>
-      <div className="w-full pt-16 pr-32">
+      <div className="lg:w-full pt-16 lg:pr-24 w-[50vw] items-center flex flex-col">
         <WinDisplay allSongs={songData} />
       </div>
     </main>
--- components/Result.tsx
+++ components/Result.tsx
@@ -4,12 +4,14 @@ import { getYoutube } from '@/lib/youtube';
 
 export default function Result({ title, artist, youtube_url }: { title: string, artist: string, youtube_url: string }) {
   return (
-    <div className="relative center flex flex-col items-center w-full py-8 px-8 gap-8">
-        <h1 className="text-4xl font-medium">
+    <div className="relative justify-center flex flex-col items-center w-full py-8 px-8 gap-8">
+        <h1 className="lg:text-4xl text-2xl text-center font-medium">
           Your Song of 2024 is:
         </h1>
-        {getYoutube({ url: youtube_url })}
-        <p className="text-2xl font-medium">
+        <div className="lg:w-[40vw] lg:h-[50vh] w-[60vw] h-[30vw]">
+          {getYoutube({ url: youtube_url })}
+        </div>
+        <p className="lg:text-2xl text-base font-medium">
           { title } - { artist }
         </p>
       </div>
